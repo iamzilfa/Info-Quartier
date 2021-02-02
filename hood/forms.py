e594b36c50b6b18e912447fcf123aa489916479f
Zilfa Cyamani <zilcyam@gmail.com>
2021-02-02 20:06:55 +0200

    add forms.py

---
+++ hood/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.forms import ModelForm,Textarea,IntegerField
+from .models import Neighbourhood,Business,Post,Profile
+
+
+class NewProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user','neighbourhoods']
+
+class NeighbourhoodForm(forms.ModelForm):
+    class Meta:
+        model=Neighbourhood
+        fields = ['hood_name','location','hood_image','description','health_tell','police_number']
+
+class NewBusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        fields=['business_name','description','email']
+
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model=Post
+        fields=['post']
