100787510a7c5e07551b5267ef78024d9d84faf4
Zilfa Cyamani <zilcyam@gmail.com>
2021-02-02 20:25:13 +0200

    urls.py

--- hood/urls.py
+++ hood/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns=[
     url('^$',views.home,name = 'home'),
     url(r'^new/profile$', views.add_profile, name='edit'),
